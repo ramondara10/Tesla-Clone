d1a8e6591d50f1cbf493081283d7f9027b694627
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2022-04-27 18:55:10 -0500

    worked on Section.js file added background image and cleaned some files

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,7 +2,18 @@ import React from "react";
 import styled from "styled-components";
 
 function Section() {
-  return <Wrap>Section</Wrap>;
+  return (
+    <Wrap>
+      <ItemText>
+        <h1>Model-S</h1>
+        <p>Order Online for Touchless Delivery</p>
+      </ItemText>
+      <ButtonGroup>
+        <LeftButton>Custom Order</LeftButton>
+        <RightButton>Existing Inventory</RightButton>
+      </ButtonGroup>
+    </Wrap>
+  );
 }
 
 export default Section;
@@ -10,5 +21,37 @@ export default Section;
 const Wrap = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: blue;
-`
+  background-image: url("/images/model-s.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+`;
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+`;
+
+const RightButton = styled(LeftButton)``;
