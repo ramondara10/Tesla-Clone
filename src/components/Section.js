f6b25ece1a541515ddd1fcba8bc947d72f0e084b
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2022-04-28 15:35:28 -0500

    worked on styled components and made images show on background

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,17 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 
-function Section() {
+function Section({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) {
   return (
-    <Wrap>
+    <Wrap bgImage={backgroundImg}>
       <ItemText>
-        <h1>Model-S</h1>
-        <p>Order Online for Touchless Delivery</p>
+        <h1>{title}</h1>
+        <p>{description}</p>
       </ItemText>
       <Buttons>
         <ButtonGroup>
-          <LeftButton>Custom Order</LeftButton>
-          <RightButton>Existing Inventory</RightButton>
+          <LeftButton>{leftBtnText}</LeftButton>
+          <RightButton>{rightBtnText}</RightButton>
         </ButtonGroup>
         <DownArrow src="/images/down-arrow.svg" />
       </Buttons>
@@ -32,6 +38,7 @@ const Wrap = styled.div`
   flex-direction: column;
   justify-content: space-between; // vertical
   align-items: center; // horizontal
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
 `;
 
 const ItemText = styled.div`
@@ -42,6 +49,9 @@ const ItemText = styled.div`
 const ButtonGroup = styled.div`
   display: flex;
   margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 const LeftButton = styled.div`
@@ -60,13 +70,16 @@ const LeftButton = styled.div`
   margin: 8px;
 `;
 
-const RightButton = styled(LeftButton)``;
+const RightButton = styled(LeftButton)`
+  background-color: white;
+  opacity: 0.65;
+  color: black;
+`;
 
 const DownArrow = styled.img`
-  margin-top: 20px;
   height: 40px;
   animation: animateDown infinite 1.5s;
+  overflow-x: hidden;
 `;
 
-const Buttons = styled.div`
-`;
+const Buttons = styled.div``;
