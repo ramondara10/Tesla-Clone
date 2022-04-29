12e6c5898000106c070e0b3187d67e180fe158ad
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2022-04-29 14:12:45 -0500

    created redux for cars and deleted some files from the features folder

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,23 +2,26 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
-import { Translate } from "@mui/icons-material";
+//import { Translate } from "@mui/icons-material";
+import { selectCars } from "../features/car/carSlice";
+import { useSelector } from "react-redux";
 
 function Header() {
   const [burgerStatus, setBurgerStatus] = useState(false);
+  const cars = useSelector(selectCars);
+  console.log(cars);
   return (
     <Container>
       <a>
         <img src="/images/logo.svg" alt="Logo Here" />
       </a>
       <Menu>
-        <a href="#">Model S</a>
-
-        <a href="#">Model Y</a>
-
-        <a href="#">Model 3</a>
-
-        <a href="#">Model X</a>
+        {cars &&
+          cars.map((car, index) => (
+            <a key={index} href="#">
+              {car}
+            </a>
+          ))}
       </Menu>
       <RightMenu>
         <a href="#">Shop</a>
@@ -29,10 +32,17 @@ function Header() {
         <CloseWrapper>
           <CustomClose onClick={() => setBurgerStatus(false)} />
         </CloseWrapper>
+        {cars &&
+          cars.map((car, index) => (
+            <li key={index}>
+              <a href="#">{car}</a>
+            </li>
+          ))}
 
         <li>
           <a href="#">Existing Inventory</a>
         </li>
+
         <li>
           <a href="#">Used Inventory</a>
         </li>
@@ -44,15 +54,6 @@ function Header() {
         </li>
         <li>
           <a href="#">Roadster</a>
-        </li>
-        <li>
-          <a href="#">Semi</a>
-        </li>
-        <li>
-          <a href="#">Utilities</a>
-        </li>
-        <li>
-          <a href="#">Test Drive</a>
         </li>
       </BurgerNav>
     </Container>
@@ -120,6 +121,7 @@ const BurgerNav = styled.div`
   flex-direction: column;
   text-align: start;
   transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
+  transition: transform 0.2s;
   li {
     padding: 15px 0;
     border-bottom: 1px solid rgba(0, 0, 0, 0.2);
